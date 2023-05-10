45fed395c09350299bf081cab977623019c7e9fd
Thiago Gomes <thiago.tgs252@gmail.com>
2023-05-10 18:58:04 -0300

    alteração da navbar

---
+++ script/mobile-navbar.js
@@ -0,0 +1,52 @@
+// MUDAR TRANSPARENCIA DA NAVBAR QUANDO ROLAR O SCROLL DO MOUSE
+
+window.addEventListener('scroll', () => {
+  document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
+})
+
+// ABRIR E FECHAR NAVBAR (RESPONSIVIDADE)
+
+class MobileNavbar {
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+          }s`);
+    });
+  }
+
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
+
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }
+    return this;
+  }
+}
+
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".nav-list",
+  ".nav-list li"
+);
+mobileNavbar.init();
