cf1a414b3c939f045060696328e186b656bf5a53
matheus-siqueir4 <lima900100@gmail.com>
2023-05-15 13:31:02 -0300

    alter e delete forms

--- code/database/Cliente.php
+++ code/database/Cliente.php
@@ -8,22 +8,24 @@ class ConnectionDB {
 
         //se o atributo não conter a conexão
         if (empty(self::$conn)) {
+
+            //abre o arquivo que contém o 
             $conn = parse_ini_file('code\config\acesso.ini');
             $localhost = $conn['localhost'];
             $database = $conn['database'];
             $user = $conn['user'];
             $password = $conn['password'];
             
+            //cria instância da conexão com o banco
             self::$conn = new PDO("mysql:host=$localhost;dbname=$database", $user, $password);
             self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         return self::$conn;
     }
 
-    //salvar e atualizar registro
+    //salvar cadastro
     public static function save($data) {
         
-        //instância com conexão do banco
         $conn = self::getConnection();
 
         $sql = "INSERT INTO cliente (nome, email, numero_contato, cpf, senha) 
@@ -40,7 +42,41 @@ class ConnectionDB {
         ]);
         return true;
     }
-
     
+    public static function alter($data) {
+
+        $conn = self::getConnection();
+
+        $sql = "UPDATE cliente SET 
+        nome = :nome,
+        email = :email,
+        numero_contato = :numero_contato,
+        cpf = :cpf,
+        senha = :senha 
+        WHERE id = :id";
+
+        $conn
+        ->prepare($sql)
+        ->execute([
+        ':id'             => $data['id'],
+        ':nome'           => $data['nome'],
+        ':email'          => $data['email'],
+        ':numero_contato' => $data['numero_contato'],
+        ':cpf'            => $data['cpf'],
+        ':senha'          => $data['senha']
+        ]);
+    }
+
+    public static function delete($id) {
+        
+        $conn = self::getConnection();
+
+        $sql = "DELETE FROM cliente WHERE id = :id";
+
+        $conn
+        ->prepare($sql)
+        ->bindParam(':id', $id)
+        ->execute();
+    }
 
 }
