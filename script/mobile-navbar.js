112f5992555e034b3c95445489b7da29c4110aa3
Thiago Gomes <thiago.tgs252@gmail.com>
2023-05-15 21:28:53 -0300

    correções

--- script/mobile-navbar.js
+++ script/mobile-navbar.js
@@ -1,9 +1,3 @@
-// MUDAR TRANSPARENCIA DA NAVBAR QUANDO ROLAR O SCROLL DO MOUSE
-
-window.addEventListener('scroll', () => {
-  document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
-})
-
 // ABRIR E FECHAR NAVBAR (RESPONSIVIDADE)
 
 class MobileNavbar {
